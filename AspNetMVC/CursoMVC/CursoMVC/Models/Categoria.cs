5d5863bc9486b2a6c94dcb2ec22f5b5aa931de6e
Aparecido789 <aparecidofranciscosilva10@gmail.com>
2020-09-27 00:29:59 -0300

    Criação da WEBAPI e integrando o Swagger para a documentação

--- AspNetMVC/CursoMVC/CursoMVC/Models/Categoria.cs
+++ AspNetMVC/CursoMVC/CursoMVC/Models/Categoria.cs
@@ -13,7 +13,6 @@ namespace CursoMVC.Models
         [Required(ErrorMessage = "O campo descrição é obrigatório")]
         public string Descricao { get; set; }
 
-        public List<Produto> Produtos { get; set; }
 
     }
 }
